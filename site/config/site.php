10b271d835358d4b4e44f7284349c51bf78a128d
Ben5 <ben-smith@blueyonder.co.uk>
2013-01-02 13:12:57 +0000

    removing the 'public' from the const definitions in site.php

--- site/config/site.php
+++ site/config/site.php
@@ -2,12 +2,12 @@
 
 class SiteConfig
 {
-    public const SITE_ROOT   = "/opt/site/site";
-    public const REVERB_ROOT = "/opt/site/reverb";
-    public const DEFAULT_HEAD_TITLE = "Terrain Generator";
+    const SITE_ROOT   = "/opt/site/site";
+    const REVERB_ROOT = "/opt/site/reverb";
+    const DEFAULT_HEAD_TITLE = "Terrain Generator";
 
-    public const DB_HOST = 'localhost';
-    public const DB_USER = 'db_user';
-    public const DB_PASS = 'wpe84u9384u5';
-    public const DB_DB   = 'default_db_name';
+    const DB_HOST = 'localhost';
+    const DB_USER = 'db_user';
+    const DB_PASS = 'wpe84u9384u5';
+    const DB_DB   = 'default_db_name';
 }
